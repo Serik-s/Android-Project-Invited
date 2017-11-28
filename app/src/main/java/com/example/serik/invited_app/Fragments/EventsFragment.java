51cbfe2626a276e84f5d868423814dbdf683f8a6
Serik-s <web.application.creater@gmail.com>
2017-11-28 21:06:15 +0600

    added firebase database

--- app/src/main/java/com/example/serik/invited_app/Fragments/EventsFragment.java
+++ app/src/main/java/com/example/serik/invited_app/Fragments/EventsFragment.java
@@ -1,6 +1,7 @@
 package com.example.serik.invited_app.Fragments;
 
 import android.content.Context;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,17 +14,28 @@ import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
-
+import com.example.serik.invited_app.Activities.LoginActivity;
 import com.example.serik.invited_app.Adapters.EventsAdapter;
 import com.example.serik.invited_app.Models.Event;
 import com.example.serik.invited_app.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Serik on 27.11.2017.
@@ -33,6 +45,7 @@ public class EventsFragment extends Fragment {
     // Store instance variables
     private String title;
     private int page;
+    Gson gson;
 
 //    @Inject
     Context mContext;
@@ -42,6 +55,7 @@ public class EventsFragment extends Fragment {
     EventsAdapter eventsAdapter;
     List<Event> eventList = new ArrayList<>();
     private static String fragmentTAG = "EVENTS FRAGMENT";
+    private static String databaseTAG = "FIREBASE DATABASE";
 
     // newInstance constructor for creating fragment with arguments
     public static EventsFragment newInstance() {
@@ -57,6 +71,12 @@ public class EventsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Events");
+
+        readData(myRef);
+
         page = getArguments().getInt("someInt", 0);
         title = getArguments().getString("someTitle");
 
@@ -69,12 +89,13 @@ public class EventsFragment extends Fragment {
         View view = inflater.inflate(R.layout.events_activity, container, false);
 //        TextView tvLabel = (TextView) view.findViewById(R.id.);
 //        tvLabel.setText(page + " -- " + title);
-        Log.e(fragmentTAG, "We are on EVENTS FRAGMENT");
-        for (int i = 0; i < 10; i++){
-            Event testEvent = new Event();
-            eventList.add(testEvent);
-        }
+//        Log.e(fragmentTAG, "We are on EVENTS FRAGMENT");
+//        for (int i = 0; i < 10; i++){
+//            Event testEvent = new Event();
+//            eventList.add(testEvent);
+//        }
         Log.e(fragmentTAG, "this is list of events " + eventList);
+
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
         eventsAdapter = new EventsAdapter(getActivity(), eventList);
@@ -85,6 +106,38 @@ public class EventsFragment extends Fragment {
 
 //            new DatabaseAsync().execute();
         return view;
+    }
+
+    private void readData(DatabaseReference myRef) {
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Log.e(databaseTAG, "this is dataSnapshot " + dataSnapshot);
+                Object value = dataSnapshot.getValue(Object.class);
+                Log.d(TAG, "Value is: " + value);
+
+                for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+                    String eventTitle = (String) messageSnapshot.child("eventTitle").getValue();
+                    String eventDate = (String) messageSnapshot.child("eventDate").getValue();
+                    String eventDescription = (String) messageSnapshot.child("eventDescription").getValue();
+                    long eventPopulation = (long) messageSnapshot.child("eventPopulation").getValue();
+
+                    Event event = new Event(eventTitle, eventDate, eventDescription, eventPopulation);
+                    Log.e(fragmentTAG, event.toString());
+                    eventList.add(event);
+                }
+                Log.e(fragmentTAG, "this is updated list of events " + eventList);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
 //
--- app/src/main/java/com/example/serik/invited_app/Models/Event.java
+++ app/src/main/java/com/example/serik/invited_app/Models/Event.java
@@ -3,6 +3,13 @@ package com.example.serik.invited_app.Models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+
 /**
  * Created by Serik on 13.11.2017.
  */
@@ -14,6 +21,7 @@ public class Event implements Parcelable {
     private String eventDescription;
     private String eventDate;
     private String imageURL;
+    private long eventPopulation;
 
     public Event() {
         eventID = 1;
@@ -22,11 +30,19 @@ public class Event implements Parcelable {
         eventDate = "30th of Novermber";
     }
 
+    public Event(String eventTitle, String eventDescription, String eventDate, long  eventPopulation){
+        this.eventTitle = eventTitle;
+        this.eventDescription = eventDescription;
+        this.eventDate = eventDate;
+        this.eventPopulation = eventPopulation;
+    }
+
     protected Event(Parcel in) {
         eventID = in.readInt();
         eventTitle = in.readString();
         eventDate = in.readString();
         eventDescription = in.readString();
+        eventPopulation = in.readLong();
         imageURL = in.readString();
     }
 
@@ -42,6 +58,8 @@ public class Event implements Parcelable {
             return new Event[size];
         }
     };
+
+
 
 
     // GET METHODS FOR EVENT
@@ -74,6 +92,11 @@ public class Event implements Parcelable {
         parcel.writeString(eventDescription);
         parcel.writeString(eventDate);
         parcel.writeString(imageURL);
+        parcel.writeLong(eventPopulation);
+    }
 
+    @Override
+    public String toString() {
+        return "This is your EVENT" + eventTitle + " " + eventDescription + " " + eventDate + " " + eventPopulation;
     }
 }
